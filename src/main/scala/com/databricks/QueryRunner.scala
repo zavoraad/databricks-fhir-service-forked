2d45e43a7e84dd0688013f5bf5c5281747d9cd0a
Aaron Z <aaron.zavora@databricks.com>
2025-01-30 10:15:01 -0800

    updates

--- src/main/scala/com/databricks/QueryRunner.scala
+++ src/main/scala/com/databricks/QueryRunner.scala
@@ -17,7 +17,7 @@ case class QueryInput(query: String)
 
 // QueryOutput case class
 //Runtime in milliseconds
-case class QueryOutput(queryResults: List[Map[String, String]], queryRuntime: Long, queryStartTime: DateTime, error: Option[List[String]]){
+case class QueryOutput(queryResults: List[Map[String, String]], queryRuntime: Long, queryStartTime: DateTime, error: Option[String]){
   override def toString : String = {
     "queryRuntime (in ms): " + queryRuntime +
     "\nqueryStartTime: " + queryStartTime +
@@ -70,7 +70,7 @@ class QueryRunner (auth: Auth){
 
       } catch {
         case e: Exception =>
-          QueryOutput(List[Map[String, String]](), System.currentTimeMillis() - queryStartTime.getMillis, queryStartTime, Some(e.getStackTrace.toList.map(_.toString)))
+          QueryOutput(List[Map[String, String]](), System.currentTimeMillis() - queryStartTime.getMillis, queryStartTime, Some(e.toString))
       }
     }
   }
