223e732cda1f9d3d8720dcb195d2199d6dfe224f
Aaron Z <aaron.zavora@databricks.com>
2025-04-18 15:53:44 -0400

    reads only

--- src/main/scala/com/databricks/ServiceManager.scala
+++ src/main/scala/com/databricks/ServiceManager.scala
@@ -4,7 +4,7 @@ class ServiceManager(val qi: QueryInterpreter, val qr: QueryRunner) {
   def read(typeSeg: String, idSeg: String, uri: Map[String, String]): FormattedOutput = {
     val sql = qi.read(typeSeg, idSeg, uri)
     val result = qr.runQuery(QueryInput(sql))
-
+    //result
     FormattedOutput.fromQueryOutputSearch(result)
   }
 }
