d755154873a2ee4d90df9935802114a71a8e882e
Aaron Z <aaron.zavora@databricks.com>
2025-07-18 12:00:49 -0400

    Empty ndjson tests

--- src/test/scala/com/databricks/industry/solutions/FormatManagerSuite.scala
+++ src/test/scala/com/databricks/industry/solutions/FormatManagerSuite.scala
@@ -15,9 +15,26 @@ class FormatManagerSuite extends BaseTest {
       queryInput = "SELECT * FROM patients"
     )
 
-    val result = FormatManager.resourceAsNDJSON(List(qo))
+    val result = FormatManager.resourcesAsNDJSON(List(qo))
     val expected = """{"resourceType":"Patient","id":"1","name":[{"family":"Test","given":["Patient"]}]}
 {"resourceType":"Patient","id":"1","name":[{"family":"Test","given":["Patient"]}]}"""
+    assert(result == expected)
+  }
+
+  test("Test empty resourceAsNDJSON") {
+    val fhirResourceJson = "" 
+
+    val queryResults = List(Map("Patient" -> fhirResourceJson), Map("Patient" -> fhirResourceJson))
+    val qo = QueryOutput(
+      queryResults = queryResults,
+      queryRuntime = 100,
+      queryStartTime = DateTime.now(),
+      error = None,
+      queryInput = "SELECT * FROM patients"
+    )
+
+    val result = FormatManager.resourcesAsNDJSON(List(qo))
+    val expected = ""
     assert(result == expected)
   }
 } 
