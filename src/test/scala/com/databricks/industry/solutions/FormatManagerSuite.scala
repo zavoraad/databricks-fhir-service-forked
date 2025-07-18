f704b92a277a8f7fe2ebf80c0aa07bd7a62bf29f
Aaron Z <aaron.zavora@databricks.com>
2025-07-18 11:55:19 -0400

    Add FormatManager and associated tests
    - Introduced FormatManager for handling FHIR resource formatting, including methods for NDJSON conversion and bundle creation.
    - Updated ServiceManager to utilize FormatManager for output formatting.
    - Added unit tests for resourceAsNDJSON functionality in FormatManagerSuite.

---
+++ src/test/scala/com/databricks/industry/solutions/FormatManagerSuite.scala
@@ -0,0 +1,23 @@
+package com.databricks.industry.solutions.fhirapi
+
+import org.joda.time.DateTime
+
+class FormatManagerSuite extends BaseTest {
+
+  test("Test resourceAsNDJSON") {
+    val fhirResourceJson = """{"resourceType": "Patient", "id": "1", "name": [{"family": "Test", "given": ["Patient"]}]}"""
+    val queryResults = List(Map("Patient" -> fhirResourceJson), Map("Patient" -> fhirResourceJson))
+    val qo = QueryOutput(
+      queryResults = queryResults,
+      queryRuntime = 100,
+      queryStartTime = DateTime.now(),
+      error = None,
+      queryInput = "SELECT * FROM patients"
+    )
+
+    val result = FormatManager.resourceAsNDJSON(List(qo))
+    val expected = """{"resourceType":"Patient","id":"1","name":[{"family":"Test","given":["Patient"]}]}
+{"resourceType":"Patient","id":"1","name":[{"family":"Test","given":["Patient"]}]}"""
+    assert(result == expected)
+  }
+} 
