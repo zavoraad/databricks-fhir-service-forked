68dcec026602c47fc4cad1c0282515ade9ed47ea
Aaron Z <aaron.zavora@databricks.com>
2025-07-29 09:08:01 -0400

    Building out search functions

--- src/main/scala/com/databricks/industry/solutions/ServiceManager.scala
+++ src/main/scala/com/databricks/industry/solutions/ServiceManager.scala
@@ -12,7 +12,22 @@ class ServiceManager(val qi: QueryInterpreter, val qr: QueryRunner) {
   }
 
   //@Gerta tie the services together of (1) build query, (2) run query, (3) return result paged
-  def search(): Unit = {
+
+  /* 
+      2 primary ways to do paged searches... 
+       (1) Run a query with an order by... range 1-100, range 101-200
+       (2) Run a query and keep a cursor open
+
+       Search
+       (1) Build Query 
+        Condition?onset=23.May.2009 => SELECT ... FROM Conidtion Where onset = '23.May.2009'
+   */
+  def search(typeSeg: String, uri: Map[String, String]): FormattedOutput = { //TODO FormattedOutput with an iterator instead of a list 
+    val sql = qi.search(typeSeg, uri) // Builds out the search query to run 
+    //run query... 
+    //return a paged result (cursor implementation)
     ???
   }
+
+  
 }
