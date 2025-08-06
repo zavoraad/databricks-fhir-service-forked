15898c9f99f2fd02fa792813889689228907bebc
Aaron Z <aaron.zavora@databricks.com>
2025-08-06 05:45:43 -0400

    default number of threads to 1 when only 1 cpu available

--- source-me.sh
+++ source-me.sh
@@ -1,2 +1,4 @@
-export JAVA_HOME=/opt/homebrew/Cellar/openjdk@17/17.0.12/libexec/openjdk.jdk/Contents/Home
-export PATH=/opt/homebrew/Cellar/openjdk@17/17.0.12/bin:${PATH}
+export JAVA_HOME=/opt/homebrew/Cellar/openjdk@17/17.0.15/libexec/openjdk.jdk/Contents/Home
+export PATH=/opt/homebrew/Cellar/openjdk@17/17.0.15/bin:${PATH}
+#export token=
+#export jdbc=
