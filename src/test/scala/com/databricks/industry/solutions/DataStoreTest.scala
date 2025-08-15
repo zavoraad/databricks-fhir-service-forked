669cf3e8c54161e707219d6008e05c0e115a11ef
Aaron Z <aaron.zavora@databricks.com>
2025-08-15 15:23:26 -0400

    resource leakage test in pool datastore

--- src/test/scala/com/databricks/industry/solutions/DataStoreTest.scala
+++ src/test/scala/com/databricks/industry/solutions/DataStoreTest.scala
@@ -15,6 +15,18 @@ class DataStoreTest extends BaseTest with BeforeAndAfter {
 
   test("PoolDataStore Connectivity"){
     assume(canConnect, "Not able to connect to a Databricks resource")
+  }
 
+  test("Resources are properly released"){
+    assume(canConnect, "Not able to connect to a Databricks resource")
+    val pool = new PoolDataStore(ta)
+    try {
+      for (i <- 1 to 100) {
+        val (_, error) = pool.execute("SELECT 1", 1, pool.getConnection)
+        assert(error.isEmpty, s"Query failed on iteration $i with error: ${error.getOrElse("")}")
+      }
+    } finally {
+      pool.disconnect
+    }
   }
 }
